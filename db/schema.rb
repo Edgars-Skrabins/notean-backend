6b1a8f3778e473be367f8352b96f3e0e94ad04e8
Gregors <gregors.las@gmail.com>
2025-02-20 19:44:56 +0100

    some user functionality
    Not finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_27_125614) do
-  create_table "workspaces", force: :cascade do |t|
+ActiveRecord::Schema[7.2].define(version: 2025_02_20_181222) do
+  create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "password"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "workspaces", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end
