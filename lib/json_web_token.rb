180ee1ec42ce39a666e9726920a1bffa3daeaf00
Edgars Skrabiņš <shiftkiro@gmail.com>
2026-09-23 13:24:50 +0300

    Added JWT encode/decode helper

---
+++ lib/json_web_token.rb
@@ -0,0 +1,13 @@
+class JsonWebToken
+  SECRET_KEY = Rails.application.credentials.secret_key_base
+
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload = payload.merge(exp: exp.to_i)
+    JWT.encode(payload, SECRET_KEY, 'HS256')
+  end
+
+  def self.decode(token)
+    decoded = JWT.decode(token, SECRET_KEY, true, algorithm: 'HS256')[0]
+    ActiveSupport::HashWithIndifferentAccess.new(decoded)
+  end
+end
